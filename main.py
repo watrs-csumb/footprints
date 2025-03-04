0bb80627637128ca62822effe615bfb3001c45dc
Robin Fishman <13447870+aetriusgx@users.noreply.github.com>
2025-03-04 19:30:10 +0000

    Footprint script supports TOML configuration.

--- main.py
+++ main.py
@@ -1,39 +1,30 @@
 from src.Footprint import Footprint
 
-import argparse
 import pathlib
+import tomllib
 
 import matplotlib.pyplot as plt
 import pandas as pd
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--input", "-i", type=str, help="Path to Ameriflux .dat file")
-parser.add_argument("--output", "-o", nargs="?", type=str, help="Path to output .csv file")
-parser.add_argument("--contour", "-c", nargs="?", help="""Percentage of source area for which to provide contours, must be between 10% and 90%.\n
-            Can be either a single value (e.g., "80") or a list of values (e.g., "[10, 20, 30]")\n
-            Expressed either in percentages ("80") or as fractions of 1 ("0.8").\n
-            Default is [90]. Set to "None" for no output of percentages""")
-parser.add_argument("--boundary-layer-height", "-b", nargs="?", type=float, help="Boundary layer height [m]")
-
 def main():
-    args = parser.parse_args()
-    
-    afdat = args.input
-    blh = args.boundary_layer_height
-    contour = args.contour
+    cfg = tomllib.load(open("app.toml", "rb"))
+    afdat = f".{cfg['input']['file']}"
+    tower_location = cfg["input"]["location"]
+    tower_spec = cfg["input"]["tower_spec"]
+    blh = cfg["input"]["boundary_layer_height"]
+    contour = cfg["input"]["source_contour_ratio"]
+    hemisphere = cfg["input"]["hemisphere"]
+    outputdir = cfg["output"]["output_dir"]
+    resolution = cfg["output"]["spatial_resolution"]
+    overlap_threshold = cfg["output"]["overlap_threshold"]
     
     if not pathlib.Path(afdat).exists():
         raise FileNotFoundError(f"File {afdat} does not exist")
     
-    df = pd.read_csv(afdat)
+    if not pathlib.Path(outputdir).exists() or not pathlib.Path(outputdir).is_dir():
+        raise FileNotFoundError(f"Directory {outputdir} does not exist")
     
-    tower_location = 36.456667, 121.382222
-    hemisphere = "North"
-    tower_spec = {
-        "zm": 2.0,
-        "z0": 0.1,
-        "d": 0.335
-    }
+    df = pd.read_csv(afdat)
     
     footprint = Footprint(tower_location, tower_spec, hemisphere)
     if blh:
@@ -41,9 +32,9 @@ def main():
     if contour:
         footprint.contour_src_pct = contour
     
-    footprint_raster = footprint.attach(df).draw().rasterize(30)
+    footprint_raster = footprint.attach(df).draw().rasterize(resolution)
     
-    polygon = footprint_raster.polygonize(0.8)
+    polygon = footprint_raster.polygonize(overlap_threshold)
     
     fig, ax = plt.subplots(figsize = (6, 6))
     assert footprint_raster.raster is not None
@@ -55,14 +46,14 @@ def main():
     ax.set_ylabel('Northing (m)')
     ax.set_title(f'Accumulated Raster\n({tower_location[0]}, {tower_location[1]})')
     fig.colorbar(im, ax=ax, label='Overlap Count')
-    plt.savefig("footprint_heat.png")
+    plt.savefig(f".{outputdir}footprint_heat.png")
     
     fig, ax = plt.subplots(figsize = (6, 6))
     polygon.plot(ax = ax, edgecolor = 'black', facecolor = 'none')
     ax.set_xlabel('Easting (m)')
     ax.set_ylabel('Northing (m)')
     ax.set_title(f'Tower Footprint Polygon\n({tower_location[0]}, {tower_location[1]})')
-    plt.savefig("footprint_polygon.png")
+    plt.savefig(f".{outputdir}footprint_polygon.png")
 
 if __name__ == "__main__":
     main()
